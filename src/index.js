91ab0861008c36d32422757d28ea47bdba5e99d6
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-03-30 20:30:52 -0400

    Implement drag and drop. Some refractoring.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const Tile = require("./Tile.js")
 
 
-let userHand = document.createElement("div")
-userHand.id = "userHand"
-document.body.appendChild(userHand)
+function createTopOrBottomHand(handId) {
+	let hand = document.createElement("div")
+	hand.id = handId
+	document.body.appendChild(hand)
+	return hand
+}
+
+function createLeftOrRightHand(handId, containerId) {
+	let hand = createTopOrBottomHand(handId)
+
+	//We will return the container for the tiles. A container is used for the left and right hands in order to vertically center the tiles.
+	let container = document.createElement("div")
+	container.id = containerId
+	hand.appendChild(container)
+
+	return container
+}
+
+
+
+
+
+
 
 
 //For testing.
@@ -14,10 +34,45 @@ console.log(tiles)
 let handTiles = tiles.slice(-14)
 console.log(handTiles)
 
+
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+
+function drag(ev) {
+	let randomClass = "randomClassForTransfer" + (2**53) * Math.random()
+	ev.target.classList.add(randomClass)
+	ev.dataTransfer.setData("randomClass", randomClass);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	let randomClass = ev.dataTransfer.getData("randomClass");
+	let elem = document.getElementsByClassName(randomClass)[0]
+	elem.classList.remove(randomClass)
+
+	let dropPosition = ev.x
+	let targetBounds = ev.target.getBoundingClientRect()
+
+	if (targetBounds.right - ev.x > targetBounds.width/2) {
+		//Dropped on left side of tile. Insert before.
+		userHand.insertBefore(elem, ev.target)
+	}
+	else {
+		//Dropped on right side of tile. Insert after.
+		userHand.insertBefore(elem, ev.target.nextElementSibling)
+	}
+}
+
+
+let userHand = createTopOrBottomHand("userHand")
+
 function drawTile(tile) {
 	let elem = document.createElement("img")
 	elem.src = tile.imageUrl
 	elem.className = "userHandTile"
+	elem.draggable = true
+	elem.addEventListener("dragstart", drag)
 	userHand.appendChild(elem)
 }
 
@@ -25,22 +80,18 @@ for (let i=0;i<handTiles.length;i++) {
 	let handTile = handTiles[i]
 	console.log(handTile)
 	drawTile(handTile)
-
-
 }
+
+userHand.addEventListener("dragover", allowDrop)
+userHand.addEventListener("drop", drop)
 
 
 let leftHandTiles = tiles.slice(-28, -14)
 
 console.log(leftHandTiles)
 
-let leftHand = document.createElement("div")
-leftHand.id = "leftHand"
-document.body.appendChild(leftHand)
 
-let leftHandContainer = document.createElement("div")
-leftHandContainer.id = "leftHandContainer"
-leftHand.appendChild(leftHandContainer)
+let leftHandContainer = createLeftOrRightHand("leftHand", "leftHandContainer")
 
 function drawLeftTile(tile) {
 	let elem = document.createElement("img")
@@ -53,8 +104,6 @@ for (let i=0;i<leftHandTiles.length;i++) {
 	let leftHandTile = leftHandTiles[i]
 	console.log(leftHandTile)
 	drawLeftTile(leftHandTile)
-
-
 }
 
 
@@ -64,13 +113,7 @@ let rightHandTiles = tiles.slice(-42, -28)
 
 console.log(rightHandTiles)
 
-let rightHand = document.createElement("div")
-rightHand.id = "rightHand"
-document.body.appendChild(rightHand)
-
-let rightHandContainer = document.createElement("div")
-rightHandContainer.id = "rightHandContainer"
-rightHand.appendChild(rightHandContainer)
+let rightHandContainer = createLeftOrRightHand("rightHand", "rightHandContainer")
 
 function drawRightTile(tile) {
 	let elem = document.createElement("img")
@@ -83,8 +126,6 @@ for (let i=0;i<rightHandTiles.length;i++) {
 	let rightHandTile = rightHandTiles[i]
 	console.log(rightHandTile)
 	drawRightTile(rightHandTile)
-
-
 }
 
 
@@ -94,9 +135,7 @@ let topHandTiles = tiles.slice(-56, -42)
 
 console.log(topHandTiles)
 
-let topHand = document.createElement("div")
-topHand.id = "topHand"
-document.body.appendChild(topHand)
+let topHand = createTopOrBottomHand("topHand")
 
 
 function drawTopTile(tile) {
@@ -110,6 +149,4 @@ for (let i=0;i<topHandTiles.length;i++) {
 	let topHandTile = topHandTiles[i]
 	console.log(topHandTile)
 	drawTopTile(topHandTile)
-
-
 }
