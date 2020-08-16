e2215d42ee14f36c1f7534bb53a46ce772f38bb1
ecc521 <42424091+ecc521@users.noreply.github.com>
2020-08-16 15:07:45 -0400

    Fix a bug where calculateNextTurn did not pass exemptions to goMahjong

--- server/Room/turnChoicesProxyHandler.js
+++ server/Room/turnChoicesProxyHandler.js
@@ -188,7 +188,7 @@ function calculateNextTurn(obj, exemptFromChecks) {
 							placement.exposed = true
 							this.messageAll([clientId], "roomActionGameplayAlert", client.getNickname() + " has placed a sequence of " + placement.tiles[0].type + "s" , {clientId, speech: "Chow"})
 							if (placement.mahjong) {
-								this.goMahjong(clientId)
+								this.goMahjong(clientId, undefined, exemptFromChecks.includes(clientId))
 							}
 							this.gameData.currentTurn.userTurn = clientId
 						}
@@ -214,7 +214,7 @@ function calculateNextTurn(obj, exemptFromChecks) {
 							let matchType = [,,"pair","pong","kong"][placement.amount]
 							this.messageAll([clientId], "roomActionGameplayAlert", client.getNickname() + " has placed a " + matchType + " of " + placement.value + " " + placement.type + "s", {clientId, speech: matchType})
 							if (placement.mahjong) {
-								this.goMahjong(clientId)
+								this.goMahjong(clientId, undefined, exemptFromChecks.includes(clientId))
 							}
 							if (placement.amount === 4) {
 								//Draw them another tile.
