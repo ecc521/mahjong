a23a881423417f19124b0ae4df18649473fe177e
ecc521 <42424091+ecc521@users.noreply.github.com>
2021-02-05 19:10:07 -0500

    Update capacitorBuild.sh
    Fix misordered build setup

--- capacitorBuild.sh
+++ capacitorBuild.sh
@@ -1,9 +1,10 @@
-npx cap add ios && npx cordova-res ios --copy #Generate the ios directory and icons (errors if doesn't exist)
-
 rm -rf capacitorDir #Purge the current capacitorDir directory so that junk can't accumulate.
 mkdir capacitorDir
 cp -r assets capacitorDir/assets
+cp -r packages capacitorDir/packages
 cp -r index.html capacitorDir/index.html
+
+npx cap add ios && npx cordova-res ios --copy #Generate the ios directory and icons (errors if doesn't exist)
 
 npx cap copy
 npx cap open ios #Open in XCode
